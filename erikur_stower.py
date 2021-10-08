60cbe395598a0213e22eb1cfefeeb51925b5435f
Erik-M-Larsson <89965231+Erik-M-Larsson@users.noreply.github.com>
2021-10-08 22:18:16 +0200

    Klass Package __init__

--- erikur_stower.py
+++ erikur_stower.py
@@ -1,24 +1,18 @@
+from math import prod
 
 class ErikurStower:
     """Klass för riktig stuveriarbetare"""
 
     def __init__(self, game_info: dict) -> None:
-        self.check_game_info(game_info)
-        self._truck = CyberTruck(game_info["vehicle"])
-        self._not_loaded_packages = game_info["dimensions"]
-
-
-    def check_game_info(self, val: any) -> None:
-        """Kontrollerar så att det är en game_info som kommer in"""
-        if isinstance(val, dict):   # Kontrollera att det är en dict
-            try:
-                val["mapName"]      # Kontrollera att rätt keys finns
-                val["vehicle"]
-                val["dimensions"]
-            except KeyError:
-                raise ValueError("Ogiltig 'game_info.")
-        else:
+        if not isinstance(game_info, dict):   # Kontrollera att det är en dict
             raise ValueError("Ogiltig 'game_info.")
+
+        try: # Testa om rätt keys finns
+            self._truck = CyberTruck(game_info["vehicle"])
+            self._not_loaded_packages = game_info["dimensions"]
+        except KeyError:
+                raise ValueError("Ogiltig 'game_info.")
+
 
 
 
@@ -33,5 +27,33 @@ class ErikurStower:
 
 class CyberTruck:
     
-    def __init__(self,vehicle) -> None:
+    def __init__(self,vehicle: dict) -> None:
         return None
+
+
+
+
+class Package:
+    """Klass för paketen"""
+
+    def __init__(self,package_data: dict) -> None:
+        if not isinstance(package_data, dict):
+            raise TypeError("package_data måste vara av typen dict")
+
+        self.id = package_data["id"]
+        self.order_class = package_data["orderClass"]
+        self.weight_class = package_data["weightClass"]
+        
+        # Sorterad lista med paketets dimensioner
+        self.dimensions = [package_data["width"], package_data["length"], package_data["height"]]      
+        self.dimensions.sort()   
+        self.volume = prod(self.dimensions)
+
+        self.loaded_on_truck = False
+        # Listor med koordinaterna på lastbilen
+        self.x18 = [0 for _ in range(8)]
+        self.y18, self.z18 = self.x18, self.x18
+
+                
+
+
